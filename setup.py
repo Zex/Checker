34606c51426f92f66cbec05c981aa088fd1a0abb
zex <top_zlynch@yahoo.com>
2015-03-29 23:54:08 +0800

    create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Zex',
+      author_email='top_zlynch@yahoo.com',
+      packages=['checker.py'],
+     )
