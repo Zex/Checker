35106661bd812faa75eb39ec269a6943e9ccc827
zex <top_zlynch@yahoo.com>
2015-03-30 00:08:31 +0800

    add checker package

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 
 setup(name='Distutils',
       version='0.1',
-      description='Python Distribution Utilities',
+      description='Checker, a Python module',
       author='Zex',
       author_email='top_zlynch@yahoo.com',
-      packages=['checker.py'],
+      packages=['checker'],
      )
