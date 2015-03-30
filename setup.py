cc453fa41c6126c54417c4bbc3cdf05376f0de46
zex <top_zlynch@yahoo.com>
2015-03-30 20:28:41 +0800

    add url, fix name

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='Distutils',
+setup(name='checker',
       version='0.1',
       description='Checker, a Python module',
       author='Zex',
       author_email='top_zlynch@yahoo.com',
+      url='https://github.com/zex/checher',
       packages=['checker'],
      )
