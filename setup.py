7aa5d33db4c35147694fcac643a1d8e997323b87
zex <top_zlynch@yahoo.com>
2015-03-30 20:52:53 +0800

    remove long desc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='checker',
       version='0.1',
       description='Checker, a Python module',
-      long_description=open('README.md').read(),
+      long_description='See Readme.md',
       author='Zex',
       author_email='top_zlynch@yahoo.com',
       url='https://github.com/zex/checher',
