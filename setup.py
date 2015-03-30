1ab3b5c8285b8dac881fcca01aa687bd6cc6d4cc
zex <top_zlynch@yahoo.com>
2015-03-30 20:58:55 +0800

    add pkg with find_packages

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='checker',
       version='0.1',
       description='Checker, a Python module',
-      long_description='See Readme.md',
+      long_description='See README.md',
       author='Zex',
       author_email='top_zlynch@yahoo.com',
       url='https://github.com/zex/checher',
       license='GPLv2',
-      packages=['checker'],
+      packages=find_packages(),
      )
