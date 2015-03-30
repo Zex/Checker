7717259414469769f9b5fba95d2f2c3af0005754
zex <top_zlynch@yahoo.com>
2015-03-30 20:46:00 +0800

    add license info

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ from distutils.core import setup
 setup(name='checker',
       version='0.1',
       description='Checker, a Python module',
+      long_description=open('README.md').read(),
       author='Zex',
       author_email='top_zlynch@yahoo.com',
       url='https://github.com/zex/checher',
+      license='GPLv2',
       packages=['checker'],
      )
